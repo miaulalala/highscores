27085109434f33670c6715f26d1a54641dbc4f27
Anna Larch <anna.larch@gmx.net>
2017-09-27 00:47:48 +0200

    composer update

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,11 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication, DatabaseMigrations;;
+    
+    public function setUp()
+    {
+        parent::setUp();
+        Artisan::call('db:seed');
+    }
 }
